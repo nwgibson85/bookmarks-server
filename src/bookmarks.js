10aac6077d753916aec66426efd978ddb6bbe270
Nicholas Gibson <Nwgibson85@gmail.com>
2019-12-06 07:06:53 -0600

    initial commit

---
+++ src/bookmarks.js
@@ -0,0 +1,18 @@
+const bookmarks = [
+    {
+        id: 1,
+        title: 'Thinkful',
+        url: 'thinkful.com',
+        description: 'Think outside the classroom',
+        rating: 4
+    },
+    {
+        id: 2,
+        title: 'Google',
+        url: 'www.google.com',
+        description: 'Where we find everything else',
+        rating: 4
+    }
+]
+
+module.exports = {bookmarks}
